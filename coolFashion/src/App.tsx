c33c29ec5f094c4a4bc250987ef2f771cde02abf
desireewp <desiree.persson@hotmail.com>
2023-05-02 16:28:40 +0200

    Some menu

--- coolFashion/src/App.tsx
+++ coolFashion/src/App.tsx
@@ -7,7 +7,7 @@ function App() {
 
   return (
     <>
-      <NavigationTop></NavigationTop>
+    
     </>
   )
 }
--- coolFashion/src/components/NavigationTop.tsx
+++ coolFashion/src/components/NavigationTop.tsx
@@ -1,25 +1,115 @@
-import './NavigationTop.css';
+import "./NavigationTop.css";
 
-
-const NavigationTop = () => { 
-    return(
-        <nav>
-            {/* Logotyp här */}
-            <p>CoolFashion</p>
+const NavigationTop = () => {
+  return (
+    <>
+      <nav>
+        {/* Logotyp här */}
+        <div className="main-items">
+          <p>CoolFashion</p>
+         
+        </div>
+      </nav>
+      <div className="dropdown">
+        <button className="dropbtn">Accessoarer</button>
+        <div className="dropdown-content">
+          <div className="dropdown-column">
+            <h3>Killar</h3>
             <ul>
-                <li >Start</li>
-                <li>Populärt</li>
-                <li className='dropdown-btn'>Kläder
-                </li>
-              
-                <li className='dropdown-btn'>Accessoarer</li>
-                <li className='dropdown-btn'>Outlet</li>
-                <li>Kontakt</li>
-                <li>FAQ</li>
+              <li>Väskor</li>
+              <li>Smycken</li>
+              <li>Klockor</li>
+              <li>Alla killaccessoarer</li>
             </ul>
-<div id='clothes-dropdown' className='dropdown-clotes'></div>
-        </nav>
-    )
- }
+          </div>
+          <div className="dropdown-column">
+            <h3>Tjejer</h3>
+            <ul>
+              <li>Väskor</li>
+              <li>Smycken</li>
+              <li>Klockor</li>
+              <li>Alla tjejaccessoarer</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div className="dropdown">
+        <button className="dropbtn">Outlet</button>
+        <div className="dropdown-content">
+        <div className="dropdown-column">
+            <h3>Killar</h3>
+            <ul>
+              <li>Kläder</li>
+              <li>Skor</li>
+              <li>Sport & träning</li>
+              <li>Alla kategorier</li>
+            </ul>
+          </div>
+          <div className="dropdown-column">
+            <h3>Tjejer</h3>
+            <ul>
+            <li>Kläder</li>
+              <li>Skor</li>
+              <li>Sport & träning</li>
+              <li>Alla kategorier</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    
+
+<nav>
+	<ul>
+		<li><a href="#">Start</a></li>
+		<li><a href="#">Populärt</a></li>
+		<li><a href="#">Kläder</a>
+			<ul>
+				<li>
+                    <h3>Killar</h3>
+                    <a href="#">T-shirts</a>
+                    <a href="#">Byxor</a>
+                    <a href="#">Skor</a>
+                    <a href="#">Alla killkläder</a>
+                    </li>
+				<li>
+                    <h3>Tjejer</h3>
+                    <a href="#">Jackor</a>
+                    <a href="#">Klänningar</a>
+                    <a href="#">Byxor & shorts</a>
+                    <a href="#">Alla tjejkläder</a>
+                    </li>
+			</ul>
+		</li>
+		<li><a href="#">Accessoarer</a>
+			<ul>
+            <li>
+                    <h3>Killar</h3>
+                    <a href="#">Photoshop</a>
+                    </li>
+				<li>
+                    <h3>Tjejer</h3>
+                    <a href="#">Populärt</a>
+                    </li>
+			</ul>
+		</li>
+		<li><a href="#">Outlet</a>
+			<ul>
+            <li>
+                    <h3>Killar</h3>
+                    <a href="#">Photoshop</a>
+                    </li>
+				<li>
+                    <h3>Tjejer</h3>
+                    <a href="#">Populärt</a>
+                    </li>
+			</ul>
+		</li>
+		<li><a href="#">Kontakt</a></li>
+		<li><a href="#">FAQ</a></li>
+	</ul>
+</nav>
+</>
+  );
+};
 
 export default NavigationTop;
