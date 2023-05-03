362585c03e5fbd550fde4d97b8cc558139250867
Kristoffer <hagelberg.kristoffer@gmail.com>
2023-05-03 09:24:22 +0200

    roboto font

--- coolFashion/src/App.tsx
+++ coolFashion/src/App.tsx
@@ -1,21 +1,18 @@
-import { useState } from 'react'
-import './App.css'
-import Landing from './views/Landgpage'
-import NavigationTop from './components/NavigationTop'
-import Footer from './components/Footer'
-import Mall from './Mall/Mall'
+import { useState } from "react";
+import "./App.css";
+import Landing from "./views/Landgpage";
+import NavigationTop from "./components/NavigationTop";
+import Footer from "./components/Footer";
+import Mall from "./Mall/Mall";
 
 function App() {
-  
-
   return (
     <>
       {/* <Landing></Landing> */}
-      <NavigationTop></NavigationTop>
+
       <Mall></Mall>
-      <Footer></Footer>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- coolFashion/src/Mall/Mall.tsx
+++ coolFashion/src/Mall/Mall.tsx
@@ -1,14 +1,17 @@
+import Footer from "../components/Footer";
+import NavigationTop from "../components/NavigationTop";
 import "./Mall.css";
 
 function Mall() {
-    return(
-        
-        <section className="frame">
-            <div className="content-container">
-                <p>Testar Roboto</p>
-            </div>
-        </section>
-    );
+  return (
+    <section className="frame">
+      <NavigationTop></NavigationTop>
+      <div className="content-container">
+        <p>Testar Roboto</p>
+      </div>
+      <Footer></Footer>
+    </section>
+  );
 }
 
 export default Mall;
