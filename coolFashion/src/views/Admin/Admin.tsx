8b1ac4446f743b0a73bce30f78fc0888f9a3c6a2
desireewp <desiree.persson@hotmail.com>
2023-05-05 11:28:18 +0200

    error handling

--- coolFashion/src/views/Admin/Admin.tsx
+++ coolFashion/src/views/Admin/Admin.tsx
@@ -1,32 +1,30 @@
 import { collection, getDocs } from 'firebase/firestore';
 import { db } from '../../../firestore-config'
 import { useEffect, useState } from 'react';
-
-const dbRef = db;
-const contactMsgCollectionRef = collection(dbRef, 'ContactMsg')
-console.log(dbRef);
-
-
-
-
-const [contactMsg, setContactMsg] = useState();
-
-useEffect(() => { 
-    const getContactMsg = async () => {
-        const contactMessagesData = await getDocs(contactMsgCollectionRef)
-        
-
-    }
- })
-
-
-
-
+import { IContactMessage } from '../../Interfaces/Interfaces';
 
 const Admin = () => { 
+const [contactMsg, setContactMsg] = useState<IContactMessage[]>([]);
 
+const dbRef = db;
+const contactMsgCollectionRef = collection(dbRef, 'contact')
 
+    const getContactMsg = async () => {
+        const contactMessagesData = await getDocs(contactMsgCollectionRef)
+        setContactMsg(
+            contactMessagesData.docs.map((doc) => ({ 
+            ...(doc.data() as IContactMessage), 
+            id: doc.id,
+         })));
+        };
+    
 
+        useEffect(() => { 
+            getContactMsg();
+            console.log(contactMsg[0]);
+         
+                
+          }, []);
 
 
     return(
