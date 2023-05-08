3d0d2012b78b3988e6d5258cfcc7d8871cd5d5f9
desireewp <desiree.persson@hotmail.com>
2023-05-08 11:06:45 +0200

    working on fileupload

--- coolFashion/src/views/Admin/Admin.tsx
+++ coolFashion/src/views/Admin/Admin.tsx
@@ -3,6 +3,8 @@ import { db } from "../../../firestore-config";
 import { useEffect, useState } from "react";
 import { IContactMessage } from "../../Interfaces/Interfaces";
 
+import "./Admin.css";
+
 const Admin = () => {
   const [contactMsg, setContactMsg] = useState<IContactMessage[]>([]);
 
@@ -23,24 +25,98 @@ const Admin = () => {
     getContactMsg();
   }, []);
 
+
+//---------------- Event handelers-----------------
+ const fileSelectedHandler = (event: React.ChangeEvent<HTMLInputElement>) => { 
+  console.log(event.target)
+  }
+
+
+//  --------------- HTML Elements -----------------
   const messageListElement = contactMsg.map((msg) => {
     return (
-      <>
-        <p key={msg.id}>{msg.message}</p>
-        <br />
-      </>
+        <p key={msg.id}>{msg.message}</p>      
     );
   });
 
-  //   Ostbågar är bra
 
   return (
     <>
       <h1>Jag är ADMIN</h1>
+      <div className="flex-row">
+        {/* Kontaktmeddelanden */}
+        <div className="container-frame">
+          <h2>Kontakt meddelanden</h2>
+          <hr />
+          {messageListElement}
+        </div>
 
-      <div>
-        <h2>Kontakt meddelanden</h2>
-        {messageListElement}
+        {/* Formulär */}
+        <div className="container-frame">
+          <h2>Lägg till ny produkt</h2>
+          <hr />
+          <form>
+            <div>
+              <label htmlFor="title">Produktnamn</label>
+              <input
+                type="text"
+                id="title"
+                placeholder="Produktnamn"
+                required
+              />
+            </div>
+            <div>
+              <label htmlFor="images">Bilder</label>
+              <input type="file" 
+              id="images" 
+              placeholder="Bilder" 
+              accept="image/x-png, image/jpeg" 
+              required  
+              onChange={fileSelectedHandler}/>
+            </div>
+
+            <div>
+              <label htmlFor="description">Beskrivning</label>
+              <textarea
+                id="description"
+                value="Fyll i beskrivningen här"
+                maxLength={500}
+                required
+              ></textarea>
+            </div>
+
+            <div>
+              <label htmlFor="category">Kategori</label>
+              <select name="category" id="category">
+                <option value="" disabled>
+                  Kategori
+                </option>
+                <option value="T-Shirts">T-Shirts</option>
+                <option value="Skjortor">Skjortor</option>
+                <option value="Linnen">Linnen</option>
+                <option value="Jackor">Jackor</option>
+                <option value="Kappor">Kappor</option>
+                <option value="tröjor">Tröjor</option>
+                <option value="Tennisskor">Tennisskor</option>
+                <option value="Skateskor">Skateskor</option>
+                <option value="Skatebrädor">Skatebrädor</option>
+                <option value="Skateskydd">Skateskydd</option>
+                <option value="Hjälmar">Hjälmar</option>
+                <option value="Skate skirts">Skate skirts</option>
+                <option value="Klänningar">Klänningar</option>
+                <option value="Blusar">Blusar</option>
+                <option value="Kjolar">Kjolar</option>
+                <option value="Boleros">Boleros</option>
+                <option value="Klackskor">Klackskor</option>
+                <option value="Pyjamaser">Pyjamaser</option>
+                <option value="Bikinis">Bikinis</option>
+                <option value="Badbyxor">Badbyxor</option>
+              </select>
+            </div>
+
+            <button type="submit">Spara</button>
+          </form>
+        </div>
       </div>
     </>
   );
