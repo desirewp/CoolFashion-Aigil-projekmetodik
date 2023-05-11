b832f0e248731d39b8e44ff9436f80a0c923977c
desireewp <desiree.persson@hotmail.com>
2023-05-11 15:52:33 +0200

    new class ProductDB

--- coolFashion/src/Classes/classes.ts
+++ coolFashion/src/Classes/classes.ts
@@ -1,8 +1,29 @@
 export class Product {
-constructor( public category: string, public description: string, public imageUrl: string, public title: string){
+  constructor(
+    public category: string,
+    public description: string,
+    public imageUrl: string,
+    public title: string
+  ) {
     this.category = category;
     this.description = description;
     this.imageUrl = imageUrl;
     this.title = title;
+  }
 }
+
+export class ProductDB {
+  constructor(
+    public id: string,
+    public category: string,
+    public description: string,
+    public imageUrl: string,
+    public title: string
+  ) {
+    this.id = id;
+    this.category = category;
+    this.description = description;
+    this.imageUrl = imageUrl;
+    this.title = title;
+  }
 }
