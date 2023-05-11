f237c807b9f552cd7ed632ecf3cca4be3fd178b4
Joel Åkergren <joel.akergren@gmail.com>
2023-05-11 15:53:15 +0200

    Contact filen

--- coolFashion/src/views/Contact/Contact.tsx
+++ coolFashion/src/views/Contact/Contact.tsx
@@ -2,12 +2,36 @@ import { useState } from "react";
 import "./Contact.css";
 import { addDoc, collection } from "@firebase/firestore";
 import { db } from "../../../firestore-config";
+import { Product } from "../../Classes/classes";
 
 const Contact = () => {
 
   const [name, setName] = useState("");
   const [mail, setMail] = useState("");
   const [message, setMessage] = useState("");
+
+
+  const shoppingCart = [
+    new Product ("Tröjor", 
+                  "Jättefin tröja, värmer gott", 
+                  "https://firebasestorage.googleapis.com/v0/b/coolfashion-9fd35.appspot.com/o/products%2Fkids-fluffy-rainbow-unicorn-novelty-slippers-size-9-3-multi-coloured_KSCHSLIUNIS_01_1024x1024.pngb6f06f9e-b2b3-4cfc-b82c-f50f61427702?alt=media&token=b70d73dd-e83d-463c-82be-630f5c0ba42d", 
+                  "Världens bästa tröja")
+  
+
+  ]
+  
+    // const objToString = JSON.stringify(shoppingCart);
+  
+    const addToCart = () => {
+      const updatedShoppingCart = [...shoppingCart, new Product("Byxor", "Fula byxor", "https://firebasestorage.googleapis.com/v0/b/coolfashion-9fd35.appspot.com/o/products%2Fkids-fluffy-rainbow-unicorn-novelty-slippers-size-9-3-multi-coloured_KSCHSLIUNIS_01_1024x1024.pngb6f06f9e-b2b3-4cfc-b82c-f50f61427702?alt=media&token=b70d73dd-e83d-463c-82be-630f5c0ba42d", "Byxa2")] 
+      const uSCToString = JSON.stringify(updatedShoppingCart)
+      localStorage.setItem("cart", uSCToString)
+    }
+  
+    
+
+
+
 
   const contactCollectionRef = collection(db, "contact");
 
@@ -19,8 +43,11 @@ const Contact = () => {
     setMessage("");
   }
 
+  
+
   return (
-    <>
+    <>      
+      <button onClick={addToCart}>Lägg i varukorg</button>
       <h1>Skriv din fråga nedan i formuläret</h1>
       <div className="contact-container">
         <form onSubmit={(e) => addNewMessage(e)}>
