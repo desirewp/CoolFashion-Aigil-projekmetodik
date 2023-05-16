ab4c3d4ee92d36150c2c9db4c225b4f5ef0b0131
desireewp <desiree.persson@hotmail.com>
2023-05-16 14:13:52 +0200

    fixed faulty merge

--- coolFashion/src/Mall/Mall.tsx
+++ coolFashion/src/Mall/Mall.tsx
@@ -14,6 +14,7 @@ import ShoppingCart from "../views/ShoppingCart/ShoppingCart";
 function Mall() {
   return (
     <section className="frame">
+      <NavigationTop></NavigationTop>
       <div className="content-container">
         <Routes>
           <Route path='/' element={<Landing />} />
